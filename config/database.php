e09434147fd89c2ac7161e53d64e59d1bcb67609
ImKiiy27 <dzakyardianto69@gmail.com>
2026-04-13 18:09:37 +0700

    pesan

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@
 // ================================================
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'bimbelku');
+define('DB_NAME', 'bimbelorion');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_CHAR', 'utf8mb4');
